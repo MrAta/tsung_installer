6958746097c3f99562f2599b559720733ed4adf3
mrata <immrata@gmail.com>
2017-11-20 22:33:17 -0500

    clone from git and remove pervious

---
+++ remove.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo rm -rf /usr/bin/tsung
+sudo rm -rf /usr/bin/tsung /usr/lib/tsung /usr/share/tsung
--- run.sh
+++ run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install build-essential debhelper erlang-nox erlang-dev python-matplotlib gnuplot libtemplate-perl
-wget http://tsung.erlang-projects.org/dist/tsung-1.7.0.tar.gz
-tar -xvzf tsung-1.7.0.tar.gz
-cd tsung-1.7.0/
+sudo apt-get install build-essential debhelper erlang-nox erlang-dev python-matplotlib gnuplot libtemplate-perl autoconf
+git clone https://github.com/mrata/tsung.git
+cd tsung
 ./configure  
 sudo make 
 sudo make install
 cd ..
-rm -rf tsung-1.7.0.tar.gz
 
