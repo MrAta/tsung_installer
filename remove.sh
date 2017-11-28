d6d6cbcd05a4de677d7e7de9e7241558bec29d66
Ata Fatahi Baarzi <immrata@gmail.com>
2017-11-28 13:07:12 -0500

    remove tsplot as well

--- remove.sh
+++ remove.sh
@@ -2,3 +2,6 @@
 
 sudo rm -rf /usr/bin/tsung
 sudo rm -rf /usr/bin/tsung /usr/lib/tsung /usr/share/tsung
+sudo rm -rf /usr/bin/tsplot
+sudo rm -rf tsplot: /usr/bin/tsplot /usr/share/man/man1/tsplot.1
+
